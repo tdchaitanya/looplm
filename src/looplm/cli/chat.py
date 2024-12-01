ded56b2adba219fcc759b9ec28c91ba9f14f469b
Chaitanya Devaguptapu <chaitanya@Chaitanyas-MacBook-Pro-2.local>
2024-12-01 15:56:55 +0530

    feature:chat minor fixes

--- src/looplm/cli/chat.py
+++
@@ -1,7 +0,0 @@
-# # src/looplm/cli/chat.py
-# from ..chat.commands import CommandHandler
-
-# def start_chat_session(config_manager=None):
-#     """Start an interactive chat session"""
-#     handler = CommandHandler()
-#     handler.start_session()
